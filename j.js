832945f022d677ca0cf5e8fb90b51316c05656dc
katelyj <kjohnston@stuy.edu>
2019-04-21 21:44:03 -0700

    photo fix

--- j.js
+++ j.js
@@ -71,15 +71,12 @@ $(document).ready(function(){
 
 //photo gallery
 
-function myFunction(imgs) {
+function myFunction(imgs, n) {
     // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
+    var str = "expandedImg" + n;
+    var expandImg = document.getElementById(str);
     // Use the same src in the expanded image as the image being clicked on from the grid
     expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
     // Show the container element (hidden with CSS)
     expandImg.parentElement.style.display = "block";
   }
